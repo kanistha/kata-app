c784a99d8e1862da60dcf3c18c5c2cd18c58b9e1
kanistha <kanistha.a@gmail.com>
2018-03-04 20:16:13 +0100

    Update Item.java

--- src/main/java/org/supermarket/Item.java
+++ src/main/java/org/supermarket/Item.java
@@ -1,4 +1,14 @@
 package org.supermarket;
 
 public class Item {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
 }
